fccdc50a940894dc94bb624181d372df3bc74746
fxlpps <felipe.lins2@aluno.cps.sp.gov.br>
2026-09-24 10:08:11 -0300

    1.3

---
+++ Felipe_Lins_Flyout/BillCipher.xaml.cs
@@ -0,0 +1,9 @@
+namespace Felipe_Lins_Flyout;
+
+public partial class BillCipher : ContentPage
+{
+	public BillCipher()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Felipe_Lins_Flyout/StanfordPines.xaml.cs
@@ -0,0 +1,9 @@
+namespace Felipe_Lins_Flyout;
+
+public partial class StanfordPines : ContentPage
+{
+	public StanfordPines()
+	{
+		InitializeComponent();
+	}
+}
